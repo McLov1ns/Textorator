d8ad456bd6c5b84c189b13aef634f8fa4e8ba3a6
sasad <sasad@MEGABOOK>
2023-11-29 11:07:21 +0700

    Добавьте файлы проекта.

---
+++ Textorator1/Model/RichTextEditorModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Microsoft.Win32;
+using Textorator1.ViewModel;
+
+namespace Textorator1.Model
+{
+    public class RichTextEditorModel : INotifyPropertyChanged
+    {
+        private string _selectedFontFamily;
+        private string _selectedFontSize;
+
+        public IEnumerable<FontFamily> FontFamilies { get; } = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+        public IEnumerable<double> FontSizes { get; } = new List<double> { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
+        public string SelectedFontFamily
+        {
+            get { return _selectedFontFamily; }
+            set
+            {
+                _selectedFontFamily = value;
+                OnPropertyChanged(nameof(SelectedFontFamily));
+            }
+        }
+
+        public string SelectedFontSize
+        {
+            get { return _selectedFontSize; }
+            set
+            {
+                _selectedFontSize = value;
+                OnPropertyChanged(nameof(SelectedFontSize));
+            }
+        }
+
+        private RichTextBox _rtbEditor = new RichTextBox();
+        public RichTextBox RtbEditor
+        {
+            get { return _rtbEditor; }
+            set
+            {
+                _rtbEditor = value;
+                OnPropertyChanged(nameof(RtbEditor));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Textorator1/ViewModel/RichTextEditorViewModel.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.ComponentModel;
+using Textorator1.Model;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Textorator1.ViewModel
+{
+    public class RichTextEditorViewModel : INotifyPropertyChanged
+    {
+        private RichTextEditorModel _model = new RichTextEditorModel();
+        private RichTextBox _docBox = new RichTextBox();
+        public RichTextBox docBox {
+            get
+            {
+                return _docBox;
+            }
+            set
+            {
+                
+                _docBox = value;
+                OnPropertyChanged("docBox");
+            }
+        }
+        public RichTextEditorModel Model
+        {
+            get { return _model; }
+            set
+            {
+                _model = value;
+                OnPropertyChanged(nameof(Model));
+            }
+        }
+
+        private RelayCommand _openCommand;
+        public RelayCommand OpenCommand
+        {
+            get
+            {
+                return _openCommand ?? (_openCommand = new RelayCommand(param => Open_Executed()));
+            }
+        }
+
+        private RelayCommand _saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                return _saveCommand ?? (_saveCommand = new RelayCommand(param => Save_Executed()));
+            }
+        }
+        public RelayCommand CenterCommand
+        {
+            get
+            {
+                return new RelayCommand(param => _centerCommand());
+            }
+        }
+
+        private void _centerCommand()
+        {
+            EditingCommands.AlignCenter.Execute(null, docBox);
+        }
+        public RelayCommand ItalicCommand
+        {
+            get
+            {
+                return new RelayCommand(param => _italic());
+            }
+        }
+
+        private void _italic()
+        {
+            EditingCommands.ToggleItalic.Execute(null, docBox);
+        }
+
+        private void Open_Executed()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
+            
+            if (ofd.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(docBox.Document.ContentStart, docBox.Document.ContentEnd);
+                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                {
+                    if (Path.GetExtension(ofd.FileName).ToLower() == ".rtf")
+                        doc.Load(fs, DataFormats.Rtf);
+                    else if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                        doc.Load(fs, DataFormats.Text);
+                    else
+                        doc.Load(fs, DataFormats.Xaml);
+                }
+            }
+        }
+        
+        private void Save_Executed()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (*.txt)|*.txt|RichText Files (*.rtf)|*.rtf|XAML Files (*.xaml)|*.xaml|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(docBox.Document.ContentStart, docBox.Document.ContentEnd);
+                using (FileStream fs = File.Create(sfd.FileName))
+                {
+                    if (Path.GetExtension(sfd.FileName).ToLower() == ".rtf")
+                        doc.Save(fs, DataFormats.Rtf);
+                    else if (Path.GetExtension(sfd.FileName).ToLower() == ".txt")
+                        doc.Save(fs, DataFormats.Text);
+                    else
+                        doc.Save(fs, DataFormats.Xaml);
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private string textInput;
+        public string TextInput
+        {
+            get
+            {
+                return textInput;
+            }
+            set
+            {
+                textInput = value;
+                OnPropertyChanged(textInput);
+            }
+        }
+
+    }
+}
